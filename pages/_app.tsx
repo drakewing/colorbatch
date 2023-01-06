cfa0a96fd9e635621d30b46e6794d4f506faebfe
Matt Drake <drake.mattc@gmail.com>
2023-01-06 15:19:32 -0800

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS theme={{}}>
       <Component {...pageProps} />
+      <Analytics />
     </MantineProvider>
   );
 }
